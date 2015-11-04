02b196d9ea248c2fdfc0a04eb3630b4b72f1b642
rafatili <rafaelchiea@gmail.com>
2015-11-04 14:28:20 -0200

    criadas as classes Csinal_in, Cdados e pasta de funções

---
+++ Csinal_in.m
@@ -0,0 +1,15 @@
+classdef Csinal_in
+    %Csinal_in Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        Tfile;
+        Nfile;
+        Angle;
+    end
+    
+    methods
+    end
+    
+end
+
