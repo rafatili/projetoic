d7567e44fd70c0989d68620d148cdac1ac7cc3d4
kauewerner <kaue.werner@gmail.com>
2015-11-13 18:04:00 -0200

    inclusao da estrategia ACE

--- funcoes/ext_env.m
+++ funcoes/ext_env.m
@@ -1,13 +1,16 @@
 function saida = ext_env(entrada,tipo,fcorte_fpb,freq_amost,ordem_fpb)
          
          if strcmp(tipo,'Hilbert') == 1
-         saida = abs(hilbert(entrada'))';
+            saida = abs(hilbert(entrada'))';
          elseif strcmp(tipo,'Retificacao') == 1
-         saida = abs(entrada);
+            saida = abs(entrada);
+            w = 2*(fcorte_fpb/freq_amost);         
+            [b,a] = butter(ordem_fpb,w);              
+            for i = 1:size(entrada,1)            
+                max_env = max(saida(i,:));
+                saida(i,:) = abs(filter(b,a,saida(i,:)));
+                saida(i,:) = (max_env/max(saida(i,:))).*saida(i,:);
+            end
          end
-         
-         w = 2*(fcorte_fpb/freq_amost);         
-         [b,a] = butter(ordem_fpb,w);         
-         %saida = filter(b,a,saida);
-         
+                         
 end
--- funcoes/ger_pulsos.m
+++ funcoes/ger_pulsos.m
@@ -1,7 +1,5 @@
 function saida = ger_pulsos(entrada,num_canais,maxima,freq_amost,T_total,taxa_est,tipo_pulso,largura_pulso,interphase_gap,fase_pulso,atraso,max_corr,quant_bits)
 
-    if num_canais == maxima % Estrategia CIS            
-            
             if strcmp(fase_pulso,'Catodico') == 1 
                 fase = 1;
             elseif strcmp(fase_pulso,'Anodico') == 1
@@ -9,7 +7,11 @@ function saida = ger_pulsos(entrada,num_canais,maxima,freq_amost,T_total,taxa_es
             end
                                     
             t = 0;
-              
+            
+%% Estrategia CIS            
+            
+      if num_canais == maxima          
+            
             if num_canais*taxa_est < freq_amost            
                 for i = 1:num_canais
                     resamp(i,:) = resample(entrada(i,:), num_canais*taxa_est,freq_amost);               
@@ -21,36 +23,61 @@ function saida = ger_pulsos(entrada,num_canais,maxima,freq_amost,T_total,taxa_es
                 resamp = entrada;
             end
             
-            a = 1;
-            %saida = zeros(num_canais,2*T_total*taxa_est,2);           
+            a = 1;        
 
                 for j = 1:num_canais:T_total*taxa_est*num_canais - num_canais + 2                   
                     n = 0;                         
                     for i = num_canais:-1:1                        
-                        %amin = min(resamp(i,:));
                         if strcmp(tipo_pulso,'Bifasico') == 1                            
-                            %if resamp(i,j + n)~=0
-                            for k = 1:2                                
-                                if k == 1
-                                saida(i,a,:) = [t , fase*(max_corr/(2^quant_bits-1))*resamp(i,j + n)]; 
-                                elseif k == 2 
+                                saida.(strcat('E',num2str(i)))(a,:) = [t , fase*(max_corr/(2^quant_bits-1))*resamp(i,j + n)]; 
                                 t = t + largura_pulso + interphase_gap;
-                                saida(i,a + 1,:) = [t , -fase*(max_corr/(2^quant_bits-1))*resamp(i,j + n)];                                
-                                end                              
-                            end
-                            %end
-                            t = t - (largura_pulso + interphase_gap) + 1/(num_canais*taxa_est);                       
-                        end
-                        
+                                saida.(strcat('E',num2str(i)))(a + 1,:) = [t , -fase*(max_corr/(2^quant_bits-1))*resamp(i,j + n)];                                
+                                t = t - (largura_pulso + interphase_gap) + 1/(num_canais*taxa_est);                       
+                        end                       
                         if atraso == 0
                            n = n + 1;
                         end                        
                     end                    
                     a = a + 2;
+                end              
+      else          
+%% Estrategia ACE              
+      
+          if maxima*taxa_est < freq_amost            
+                for i = 1:num_canais
+                    resamp(i,:) = resample(entrada(i,:), num_canais*taxa_est,freq_amost);               
                 end
-                a
-    else                
-    error('Estrategia ACE ainda nao programada!') % Estrategia ACE            
-    stop
-    end
+          elseif maxima*taxa_est > freq_amost
+                error('Erro: frequencia de amostragem menor do que o produto do numero de maxima pela taxa de estimulacao!')
+                stop
+          else
+                resamp = entrada;
+          end
+            
+           for i = num_canais:-1:1 
+               a(i) = 1;
+           end                 
+                for j = 1:maxima:T_total*taxa_est*num_canais - maxima + 2                                          
+                    
+                    maxima_vet = zeros(1,num_canais);
+                    for i = num_canais:-1:1 
+                       maxima_vet(i) = resamp(i,j);
+                    end
+                       [maxima1, maxima2] = sort(maxima_vet,'descend');
+                       maxima_valor = maxima1(1:maxima);
+                       maxima_canal = maxima2(1:maxima);
+                       
+                    for i = maxima:-1:1                        
+                        if strcmp(tipo_pulso,'Bifasico') == 1                            
+                                saida.(strcat('E',num2str(maxima_canal(i))))(a(maxima_canal(i)),:) = [t , fase*(max_corr/(2^quant_bits-1))*maxima_valor(i)]; 
+                                t = t + largura_pulso + interphase_gap;
+                                saida.(strcat('E',num2str(maxima_canal(i))))(a(maxima_canal(i)) + 1,:) = [t , -fase*(max_corr/(2^quant_bits-1))*maxima_valor(i)];                                
+                                t = t - (largura_pulso + interphase_gap) + 1/(num_canais*taxa_est);                       
+                                a(maxima_canal(i)) = a(maxima_canal(i)) + 2;
+                        end                     
+                    end
+                    display('Numero de pulsos por canal ACE')
+                    a
+                end          
+     end
 end
