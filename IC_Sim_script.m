085f771c925f3a26a904681de7cffa25df375873
kauewerner <kaue.werner@gmail.com>
2015-11-16 13:57:46 -0200

    mensagem

--- IC_Sim_script.m
+++ IC_Sim_script.m
@@ -9,7 +9,7 @@ addpath(genpath('./SRMRToolbox-master'))
 %sim = Cprocessador('sinal16k.wav', './dados_pacientes/p_cochlear_5.dat');
 sim = Cprocessador('sinal16k.wav', 'media');
 sim.num_canais = 22;
-sim.maxima = 8;
+sim.maxima = 10;
 sim.taxa_est = 1200;
 sim.tipo_env = 'Hilbert';
 %sim.tipo_env = 'Retificacao';
@@ -43,8 +43,8 @@ xlim([0 3.5])
 canal_min = 1;
 canal_max = 22;
 figure(2)
-for n = canal_max:-1:canal_min
-h = subplot(canal_max-canal_min+1,1,canal_max-n+1);
+for n = 1:canal_max
+h = subplot(canal_max-canal_min+1,1,n);
 vn = strcat('E',num2str(n));
 tc = sim.Csinal_processador.corr_onda.(vn);
 plot(tc(:,1),tc(:,2))
@@ -52,9 +52,19 @@ xlim([0 max(sim.vet_tempo)])
 ylim([0 sim.max_corr])
 set(h,'XTick',[])
 set(h,'YTick',[])
-ylabel(strcat('E',num2str(canal_max-n+1)))
+ylabel(strcat('E',num2str(n)))
 end
 
+%% Espectrograma
 
+[y, f, t, p] = spectrogram(sim.Csinal_processador.in,1024,128,1024,sim.freq_amost,'yaxis');
+figure(3);
+surf(t,f,10*log10(abs(p)),'EdgeColor','none');
+axis xy; 
+axis tight;
+colormap(jet);
+view(0,90);
+ylabel('Frequency (Hz) ');
+xlabel('Time ');
 
 
