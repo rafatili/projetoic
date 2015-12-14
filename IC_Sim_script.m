ac9ca2b13d381f7ca0a2d0da79393440c242b7d7
kauewerner <kaue.werner@gmail.com>
2015-12-14 17:17:28 -0200

    mudancas da apresentacao

--- IC_Sim_script.m
+++ IC_Sim_script.m
@@ -6,11 +6,11 @@ clc
 addpath(genpath('./funcoes'))
 addpath(genpath('./SRMRToolbox-master'))
 
-sim = Cprocessador('sinal16k.wav', './dados_pacientes/p_cochlear_padrao.dat');
-%sim = Cprocessador('sinal16k.wav', 'media');
+%sim = Cprocessador('sinal16k.wav', './dados_pacientes/p_cochlear_padrao.dat');
+sim = Cprocessador('sinal16k.wav', 'media');
 sim.num_canais = 22;
-sim.maxima = 10;
-sim.taxa_est = 1000;
+sim.maxima = 22;
+sim.taxa_est = 700;
 sim.tipo_env = 'Hilbert';
 %sim.tipo_env = 'Retificacao';
 %sim.fat_comp = 0.2;
@@ -25,7 +25,7 @@ display(sim.Csinal_processador)
 
 %% Etapas dos sinais
 
-n = 16;
+n = 1;
 
 figure(1)
 subplot(4,1,1)
@@ -38,27 +38,63 @@ subplot(4,1,3)
 plot(sim.vet_tempo,sim.Csinal_processador.comp(n,:))
 xlim([0 max(sim.vet_tempo)])
 subplot(4,1,4)
-vn = strcat('E',num2str(sim.num_canais-n+1));
+vn = strcat('E',num2str(n));
+tc = sim.Csinal_processador.corr_onda.(vn);
+plot(tc(:,1),tc(:,2))
+xlim([0 max(sim.vet_tempo)])
+
+n = 8;
+
+figure(2)
+subplot(4,1,1)
+plot(sim.vet_tempo,sim.Csinal_processador.filt(n,:))
+xlim([0 max(sim.vet_tempo)])
+subplot(4,1,2)
+plot(sim.vet_tempo,sim.Csinal_processador.env(n,:))
+xlim([0 max(sim.vet_tempo)])
+subplot(4,1,3)
+plot(sim.vet_tempo,sim.Csinal_processador.comp(n,:))
+xlim([0 max(sim.vet_tempo)])
+subplot(4,1,4)
+vn = strcat('E',num2str(n));
+tc = sim.Csinal_processador.corr_onda.(vn);
+plot(tc(:,1),tc(:,2))
+xlim([0 max(sim.vet_tempo)])
+
+n = 16;
+
+figure(3)
+subplot(4,1,1)
+plot(sim.vet_tempo,sim.Csinal_processador.filt(n,:))
+xlim([0 max(sim.vet_tempo)])
+subplot(4,1,2)
+plot(sim.vet_tempo,sim.Csinal_processador.env(n,:))
+xlim([0 max(sim.vet_tempo)])
+subplot(4,1,3)
+plot(sim.vet_tempo,sim.Csinal_processador.comp(n,:))
+xlim([0 max(sim.vet_tempo)])
+subplot(4,1,4)
+vn = strcat('E',num2str(n));
 tc = sim.Csinal_processador.corr_onda.(vn);
 plot(tc(:,1),tc(:,2))
 xlim([0 max(sim.vet_tempo)])
 
 %% Eletrodograma
 
-% canal_min = 1;
-% canal_max = 22;
-% figure(2)
-% for n = 1:canal_max
-% h = subplot(canal_max-canal_min+1,1,n);
-% vn = strcat('E',num2str(n));
-% tc = sim.Csinal_processador.corr_onda.(vn);
-% plot(tc(:,1),tc(:,2))
-% xlim([0 max(sim.vet_tempo)])
-% ylim([0 sim.max_corr])
-% set(h,'XTick',[])
-% set(h,'YTick',[])
-% ylabel(strcat('E',num2str(n)))
-% end
+canal_min = 1;
+canal_max = 22;
+figure(4)
+for n = 1:canal_max
+h = subplot(canal_max-canal_min+1,1,n);
+vn = strcat('E',num2str(n));
+tc = sim.Csinal_processador.corr_onda.(vn);
+plot(tc(:,1),tc(:,2))
+xlim([0 max(sim.vet_tempo)])
+ylim([0 sim.max_corr])
+set(h,'XTick',[])
+set(h,'YTick',[])
+ylabel(strcat('E',num2str(n)))
+end
 
 %% Espectrograma FFT
 
@@ -78,17 +114,17 @@ xlim([0 max(sim.vet_tempo)])
 
 %% Espectrograma Wavelet
 
-% figure(4)
-% level = 6;
-% wpt = wpdec(sim.Csinal_processador.in,level,'sym8');
-% [Spec,Time,Freq] = wpspectrum(wpt,sim.freq_amost);%,'plot');
-% surf(Time,fliplr(Freq),10*log10(abs(Spec)),'EdgeColor','none');
-% axis xy; 
-% axis tight;
-% %set(gca,'Yscale','log')
-% % set(gca,'XTick',[])
-% % set(gca,'YTick',[])
-% % ylim([2e2 8e3])
-% colormap(jet);
-% view(0,90);
+figure(5)
+level = 6;
+wpt = wpdec(sim.Csinal_processador.in,level,'sym8');
+[Spec,Time,Freq] = wpspectrum(wpt,sim.freq_amost);%,'plot');
+surf(Time,fliplr(Freq),10*log10(abs(Spec)),'EdgeColor','none');
+axis xy; 
+axis tight;
+%set(gca,'Yscale','log')
+% set(gca,'XTick',[])
+% set(gca,'YTick',[])
+% ylim([2e2 8e3])
+colormap(jet);
+view(0,90);
 
